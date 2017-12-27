5aa7e09f0d375e9c57e30b19a116f5f8ec47b649
Mao Yufeng <lcdsmao@gmail.com>
2017-12-27 11:31:42 +0900

    Update to use SensorManager

--- app/src/main/java/com/paranoid/mao/tsnddemo/tsnd/SensorCommunicationService.kt
+++ app/src/main/java/com/paranoid/mao/tsnddemo/tsnd/SensorCommunicationService.kt
@@ -6,10 +6,7 @@ import android.os.*
 import android.widget.Toast
 import com.paranoid.mao.tsnddemo.R
 import jp.walkmate.tsndservice.Listener.TSNDConnectionListener
-import jp.walkmate.tsndservice.Service.Impl.TSNDServiceImpl
-import jp.walkmate.tsndservice.Service.TSNDService
 import jp.walkmate.tsndservice.Thread.TSNDConnectionThread
-
 
 class SensorCommunicationService : Service() {
 
@@ -17,17 +14,14 @@ class SensorCommunicationService : Service() {
     val ACTION_DISCONNECT = "Disconnect"
 
     private val binder: IBinder = LocalBinder()
-    private var sensorService: TSNDService? = null
+    private val sensorService = SensorManager("Test", "00:07:80:76:87:6E")
     var isMeasuring = false
-    var isConnected: Boolean = false
-        get() = sensorService?.isConnected ?: false
+    var sensorData: SensorData = SensorData()
+        get() = sensorService.data
+        private set
 
     inner class LocalBinder: Binder() {
         fun getService(): SensorCommunicationService = this@SensorCommunicationService
-    }
-
-    override fun onCreate() {
-        sensorService = TSNDServiceImpl("00:07:80:76:87:6E")
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -47,55 +41,44 @@ class SensorCommunicationService : Service() {
     }
 
     fun connectAll() {
-        sensorService?.let {
-            val connectionThread = TSNDConnectionThread(sensorService)
-            connectionThread.setConnectionListener(object : TSNDConnectionListener {
-                override fun onConnected() {
-                    showToast(R.string.msg_connect_success)
-                }
+        val connectionThread = TSNDConnectionThread(sensorService)
+        connectionThread.setConnectionListener(object : TSNDConnectionListener {
+            override fun onConnected() {
+                showToast(R.string.msg_connect_success)
+            }
 
-                override fun onFailedToConnect() {
-                    showToast(R.string.msg_connect_failed)
-                }
+            override fun onFailedToConnect() {
+                showToast(R.string.msg_connect_failed)
+            }
 
-            })
-            connectionThread.start()
-        }
+        })
+        connectionThread.start()
     }
 
     fun disconnectAll() {
-        sensorService?.disconnect()
+        if (sensorService.isConnected) {
+            sensorService.disconnect()
+        }
     }
 
     fun startMeasure() {
-        sensorService?.let {
-            if (it.isConnected) {
-                isMeasuring = true
-                it.run()
-            }
+        if (sensorService.isConnected && !isMeasuring) {
+            isMeasuring = true
+            sensorService.run()
         }
     }
 
     fun stopMeasure() {
-        sensorService?.let {
-            if (it.isConnected) {
-                isMeasuring = false
-                it.stop()
-            }
+        if (sensorService.isConnected && isMeasuring) {
+            isMeasuring = false
+            sensorService.stop()
         }
     }
 
-    fun getSensorData(): SensorData = sensorService?.run {
-            SensorData(0, accX, accY, accZ, gyrX, gyrY, gyrZ)
-        } ?: SensorData()
-
     override fun onDestroy() {
-        super.onDestroy()
+        stopMeasure()
         disconnectAll()
-    }
-
-    private fun showToast(text: String) = Handler(Looper.getMainLooper()).apply {
-        post { Toast.makeText(this@SensorCommunicationService, text, Toast.LENGTH_SHORT).show() }
+        super.onDestroy()
     }
 
     private fun showToast(textId: Int) = Handler(Looper.getMainLooper()).apply {
