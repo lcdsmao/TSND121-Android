f2989c0ece98405774f5ba1ffe23b3e1fa1c30c2
Mao Yufeng <lcdsmao@gmail.com>
2017-12-27 11:27:25 +0900

    create sensor data saver to save to csv file

--- app/src/main/java/com/paranoid/mao/tsnddemo/tsnd/SensorData.kt
+++ app/src/main/java/com/paranoid/mao/tsnddemo/tsnd/SensorData.kt
@@ -7,4 +7,5 @@ data class SensorData(val time: Int = 0,
                       val accX: Int = 0, val accY: Int = 0, val accZ: Int = 0,
                       val gyroX: Int = 0, val gyroY: Int = 0, val gyroZ: Int = 0,
                       val magX: Int = 0, val magY: Int = 0, val magZ: Int = 0) {
+    fun toList() = listOf<Int>(time, accX, accY, accZ, gyroX, gyroY, gyroZ, magX, magX, magZ)
 }
---
+++ app/src/main/java/com/paranoid/mao/tsnddemo/tsnd/SensorDataSaver.kt
@@ -0,0 +1,37 @@
+package com.paranoid.mao.tsnddemo.tsnd
+
+import android.util.Log
+import com.paranoid.mao.tsnddemo.csv.SimpleCSVFormat
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+
+/**
+ * Created by Paranoid on 12/26/17.
+ */
+class SensorDataSaver(name: String = "") {
+
+    private val dateFormat = SimpleDateFormat("yyyyMMddHHmmss", Locale.US)
+    private val folder = File("sdcard/TSNDDemo/")
+    private val csvFormat = SimpleCSVFormat(10)
+
+    private var fileName: String = name
+          get() = dateFormat.format(Calendar.getInstance().time) + field + ".csv"
+    private var sensorDataFile: File? = null
+
+    init {
+        createFile()
+    }
+
+    @Synchronized
+    fun recordData(data: SensorData) {
+        sensorDataFile?.appendText(csvFormat.format(data.toList()))
+    }
+
+    private fun createFile() {
+        folder.mkdirs()
+        sensorDataFile = File(folder, fileName)
+        sensorDataFile?.appendText(csvFormat.format(
+                listOf("time(msec)", "Ax", "Ay", "Az", "Gx", "Gy", "Gz", "Mx", "Mz", "My", "Mz")))
+    }
+}
