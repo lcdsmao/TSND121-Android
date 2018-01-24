f97a19dff38de7118662b284ee03669ddbe434fb
Mao Yufeng <lcdsmao@gmail.com>
2018-01-24 17:48:59 +0900

    Update SimpleCSVFormat

--- app/src/main/java/com/paranoid/mao/tsnddemo/csv/SimpleCSVFormat.kt
+++ app/src/main/java/com/paranoid/mao/tsnddemo/csv/SimpleCSVFormat.kt
@@ -1,18 +1,14 @@
 package com.paranoid.mao.tsnddemo.csv
 
-import java.util.*
+import java.lang.IllegalArgumentException
+import kotlin.math.max
 
 /**
  * Created by Paranoid on 12/26/17.
  */
 class SimpleCSVFormat(private val numOfColumns: Int) {
-    fun format(list: List<*>): String {
-        val sb = StringBuilder()
-        for (i in 0 until numOfColumns) {
-            val str = if (i < numOfColumns) list[i].toString() else ""
-            val split = if (i < numOfColumns - 1) "," else "\n"
-            sb.append(str + split)
-        }
-        return sb.toString()
+    fun format(list: List<Any>): String {
+        if (numOfColumns < list.size) throw IllegalArgumentException("list size too large")
+        return list.joinToString(separator = ",") + ",".repeat(max(numOfColumns - list.size - 1, 0)) + '\n'
     }
 }
