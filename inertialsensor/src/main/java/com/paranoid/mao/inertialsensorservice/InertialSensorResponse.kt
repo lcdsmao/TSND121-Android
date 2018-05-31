8a868f6ca9d3b4d69e8a2c911dc3326e1b8ea048
Mao <lcdsmao@gmail.com>
2018-05-31 13:40:30 +0900

    Create IMU service interface

--- inertialsensor/src/main/java/com/paranoid/mao/inertialsensorservice/InertialSensorResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.paranoid.mao.inertialsensorservice
-
-sealed class InertialSensorResponse(val message: String? = null)
-
-object StartMeasure : InertialSensorResponse()
-object StopMeasure : InertialSensorResponse()
-class SensorStatus(message: String) : InertialSensorResponse(message)
-class BatteryStatus(message: String) : InertialSensorResponse(message)
-class Error(message: String) : InertialSensorResponse(message)
--- inertialsensor/src/main/java/com/paranoid/mao/inertialsensorservice/InertialSensorService.kt
+++ inertialsensor/src/main/java/com/paranoid/mao/inertialsensorservice/InertialSensorService.kt
@@ -2,18 +2,10 @@ package com.paranoid.mao.inertialsensorservice
 
 import io.reactivex.Completable
 import io.reactivex.Flowable
-import io.reactivex.Maybe
 
 interface InertialSensorService {
-
-    enum class ServiceStatus {
-        IDLE,
-        COMMAND,
-        ONLINE_MEASURE,
-    }
-
-    fun connect(): Flowable<InertialSensorResponse>
-    fun disconnect()
-    fun startMeasure(): Flowable<InertialSensorData>
-    fun stopMeasure()
+    fun connect(): Flowable<InertialSensorData>
+    fun disconnect(): Completable
+    fun startMeasure(): Completable
+    fun stopMeasure(): Completable
 }
