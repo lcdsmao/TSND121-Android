545cff52743122f9455544dd46d960a387340e22
Mao <lcdsmao@gmail.com>
2018-06-08 19:44:25 +0900

    Clean files

--- app/src/main/java/com/paranoid/mao/tsnddemo/vo/SensorResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.paranoid.mao.tsnddemo.vo
-
-sealed class SensorResponse(open val sensor: Sensor) {
-    data class Command(override val sensor: Sensor) : SensorResponse(sensor)
-    data class Measuring(override val sensor: Sensor) : SensorResponse(sensor)
-    data class Offline(override val sensor: Sensor) : SensorResponse(sensor)
-}
