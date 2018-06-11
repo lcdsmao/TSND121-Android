cf39e66eefe8b7a5dd27ef796b451a46ec6a4324
Mao <lcdsmao@gmail.com>
2018-06-11 13:57:06 +0900

    Fix bugs in data

--- atrwirelesssensor/src/main/java/com/paranoid/mao/atrsensorservice/AccGyroMagData.kt
+++ atrwirelesssensor/src/main/java/com/paranoid/mao/atrsensorservice/AccGyroMagData.kt
@@ -4,5 +4,5 @@ data class AccGyroMagData(val time: Int = 0,
                           val accX: Int = 0, val accY: Int = 0, val accZ: Int = 0,
                           val gyroX: Int = 0, val gyroY: Int = 0, val gyroZ: Int = 0,
                           val magX: Int = 0, val magY: Int = 0, val magZ: Int = 0) {
-    fun toList() = listOf(time, accX, accY, accZ, gyroX, gyroY, gyroZ, magX, magX, magZ)
+    fun toList() = listOf(time, accX, accY, accZ, gyroX, gyroY, gyroZ, magX, magY, magZ)
 }
